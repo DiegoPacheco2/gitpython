493d59cf104ad7502ae2d7aa3907306704b1ec73
Diego Rodrigues da Silva <diegopachecosilva2@gmail.com>
2019-03-07 16:24:31 -0300

    Adicionei novos codigos no modulosvideo24(edicao)

--- módulosvideo24.py
+++ módulosvideo24.py
@@ -1,2 +1,9 @@
+from math import sqrt,ceil,floor,trunc
+num=float(input('Escreva um número: '))
+raiz=sqrt(num)
+print('mostre a raiz quadrade de {}: {}'.format(num,trunc(raiz)))
+from random import randint
+num2=randint(1,80)
+print(num2)
 import emoji
-print(emoji.emojize('Vou sair correndo :runner:', use_aliases=True))
+print(emoji.emojize('Deicha um joia e um beijo! :thumbsup: :kissing_heart:',use_aliases=True))
